ba15e7bbcfbce8ec11b5cd1ebc6ded0e7359be80
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-30 08:24:24 +0000

    Deploy STEP-tw/gol-kukshalkanishka to github.com/STEP-tw/gol-kukshalkanishka.git:gh-pages

---
+++ precache-manifest.f04392d2e9da5d18941f2c7b7d4d46ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3c413de5be287b96db3",
+    "url": "/gol-kukshalkanishka/static/js/runtime~main.a3c413de.js"
+  },
+  {
+    "revision": "511d1ce44ef0705f8d20",
+    "url": "/gol-kukshalkanishka/static/js/main.511d1ce4.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/gol-kukshalkanishka/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "511d1ce44ef0705f8d20",
+    "url": "/gol-kukshalkanishka/static/css/main.c6f3f259.chunk.css"
+  },
+  {
+    "revision": "c8c6df357edef8bd006af8084a613d91",
+    "url": "/gol-kukshalkanishka/index.html"
+  }
+];
